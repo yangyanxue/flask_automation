243fc3508b603580886250e268e8a3bc7a675af8
yangyanxue <yyxyangyanxue@163.com>
2019-02-17 19:39:04 +0800

    添加链接数据库的文件

--- flask_automation/base_tools/PyMysql.py
+++ flask_automation/base_tools/PyMysql.py
@@ -4,3 +4,40 @@
 # @Author  : yangyanxue
 # @File    : PyMysql.py
 # @Software: PyCharm
+import pymysql
+class MysqlHander:
+    def __init__(self,host,username,password,databasename,port=3306):
+        self.host = host
+        self.username = username
+        self.password =password
+        self.databasename =databasename
+        self.port = int(port)
+        self.conn = None
+    def _conn(self):
+        if self.conn is  None:
+            self.conn = pymysql.connect(
+                host = self.host,
+                port = self.port,
+                user = self.username,
+                password = self.password,
+                db = self.databasename,
+                charset = 'utf8mb4',
+                cursorclass = pymysql.cursors.DictCursor
+                )
+
+    def _dis_conn(self):
+        if self.conn is not None:
+            try:
+                self.conn = None
+                self.conn.close()
+            except Exception as _:
+                pass
+    def get_query_results(self,*args,**kwargs):
+        try:
+            self._conn()
+            with self.conn.cursor() as cursor:
+                cursor.execute(*args,**kwargs)
+                result = cursor.fetchall()
+                return result
+        finally:
+            self._dis_conn()
