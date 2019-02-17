c84c8528fcc7e95e7da43db08878eebfff67b9d0
yangyanxue <yyxyangyanxue@163.com>
2019-02-17 17:36:20 +0800

    创键pipenv环境

---
+++ flask_automation/base_tools/PyMysql.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/17 17:17
+# @Author  : yangyanxue
+# @File    : PyMysql.py
+# @Software: PyCharm
---
+++ flask_automation/view.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/17 17:17
+# @Author  : yangyanxue
+# @File    : view.py
+# @Software: PyCharm
+
+
